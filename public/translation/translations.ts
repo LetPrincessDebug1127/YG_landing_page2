cc758f9036ca442cffba2acc749e8867a8cad39a
Ngan <phamngocthungan20030@gmail.com>
2025-01-26 15:32:20 +0700

    responsive slide3

--- public/translation/translations.ts
+++ public/translation/translations.ts
@@ -11,7 +11,9 @@ const translations = {
     SERVICES : "SERVICES",
     PROJECTS : "PROJECTS",
     CLIENTS : "CLIENTS",
-    CONTACT : "CONTACT"
+    CONTACT : "CONTACT",
+    text : "We are a company specializing in providing event organization services, brand activation, decoration, and more. With a young, enthusiastic, and creative team, led by experienced individuals in the field, YG constantly affirms its unique identity, fostering the innovative mindset and youthful energy to bring true value to every task we perform for our clients.",
+    textP : "Let YG accompany you and create unique value by building an inspiring journey of creativity through emotions."
   },
   vi: {
     home: "Trang chủ",
@@ -25,7 +27,9 @@ const translations = {
     SERVICES : "DỊCH VỤ",
     PROJECTS : "DỰ ÁN",
     CLIENTS : "KHÁCH HÀNG",
-    CONTACT : "LIÊN HỆ"
+    CONTACT : "LIÊN HỆ",
+    text : "Là đơn vị chuyên cung cấp các dịch vụ tổ chức sự kiện, kích hoạt cho thương hiệu, trang trí… Với đội ngũ nhân sự trẻ trung, nhiệt huyết, đầy sáng tạo, cùng những người dẫn dắt nhiều kinh nghiệm trong lĩnh vực này. Bên cạnh đó, YG không ngừng khẳng định bản sắc riêng, phát huy tư duy đổi mới đầy nhiệt huyết của sức trẻ nhằm mang đến những giá trị thật trong từng công việc mà chúng tôi thực hiện cho khách hàng",
+    textP : "Hãy để YG được đồng hành và cùng tạo ra những giá trị khác biệt thông qua việc xây dựng hành trình trải nghiệm đầy cảm hứng sáng tạo từ cảm xúc."
   },
 };
 
--- src/components/Slide3.tsx
+++ src/components/Slide3.tsx
@@ -1,6 +1,11 @@
+"use client"
 import React from "react";
+import translations from "../../public/translation/translations";
+import { useLanguage } from "../context/LanguageContext";
 
 const VideoBackgroundSection: React.FC = () => {
+  const { language } = useLanguage();
+  const t = translations[language]; 
   return (
     <section className="relative w-full h-screen flex items-center justify-center">
       <video
@@ -11,13 +16,54 @@ const VideoBackgroundSection: React.FC = () => {
         muted
         playsInline
       ></video>
+        <div className="relative z-10 w-full h-full flex">
+        {/* Phần bên trái - Hình ảnh */}
+        <div className="w-[50%] h-full flex items-center justify-center mt-4">
+          <img
+            src="/media/banner-turning.jpg" 
+            alt="Banner"
+            className="w-full sm:w-[90%] md:w-[80%] lg:w-[80%] xl:w-[70%] 2xl:w-[60%] 3xl:w-[50%] max-w-[100%] h-auto rounded-lg shadow-lg"
+          />
+        </div>
 
-      {/* Overlay (optional)
-      <div className="absolute top-0 left-0 w-full h-full bg-black/50"></div> */}
+        {/* Phần bên phải - Văn bản */}
+        <div className="flex flex-col justify-center items-start text-white w-fit h-auto p-3">
+        <h1 className="text-[24px] sm:text-[36px] md:text-[40px] lg:text-[44px] xl:text-[40px] 2xl:text-[60px] font-bold mb-4 whitespace-nowrap">
+            Young Generation Agency
+        </h1>
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl mb-3 lg:w-[30em] xl:w-[27em] 2xl:w-[40em] text-justify">
+        {t.text}
+        </p>
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl lg:w-[30em] xl:w-[27em] 2xl:w-[40em] text-justify">
+        {t.textP}
+        </p>
+
+        </div>
 
 
+      </div>
     </section>
+    
   );
 };
 
 export default VideoBackgroundSection;
+//    <div>
+//                 <img src="/media/banner-turning" />
+//             </div>
+//             <div >
+//                 <h1>Young Generation Agency</h1>
+//                 <p>
+                // Là đơn vị chuyên cung cấp các dịch vụ tổ chức sự kiện, kích hoạt cho
+                // thương hiệu, trang trí… Với đội ngũ nhân sự trẻ trung, nhiệt huyết, đầy
+                // sáng tạo, cùng những người dẫn dắt nhiều kinh nghiệm trong lĩnh vực này.
+                // Bên cạnh đó, YG không ngừng khẳng định bản sắc riêng, phát huy tư duy đổi
+                // mới đầy nhiệt huyết của sức trẻ nhằm mang đến những giá trị thật trong
+                // từng công việc mà chúng tôi thực hiện cho khách hàng.
+//                 </p>
+//                 <p>
+                // Hãy để YG được đồng hành và cùng tạo ra những giá trị khác biệt thông qua
+                // việc xây dựng hành trình trải nghiệm đầy cảm hứng sáng tạo từ cảm xúc.
+//                 </p>
+//             </div>
+//             </div>
